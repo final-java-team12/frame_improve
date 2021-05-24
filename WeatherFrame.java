4e07766d510bae4edfefe9d7c19866e86b816636
vivian-usr <73873274+vivian-usr@users.noreply.github.com>
2021-05-24 21:46:33 +0800

    Add files via upload

--- WeatherFrame.java
+++ WeatherFrame.java
@@ -10,6 +10,7 @@ import java.awt.GridLayout;
 import java.awt.BorderLayout;
 import javax.swing.JPanel;
 import java.awt.Font;
+import javax.swing.JComboBox;
 
 //產生點選雲時介面
 public class WeatherFrame extends JFrame{
@@ -18,7 +19,10 @@ public class WeatherFrame extends JFrame{
     private JButton goSearch;
     private JPanel p;
     private JPanel otherp;
-
+    private final JComboBox<String> cityComboBox;
+    private static final String[] city =
+            {"基隆市", "台北市", "新北市","桃園市","新竹市","新竹縣","苗栗縣","台中市","彰化縣","南投縣"
+            ,"雲林縣","嘉義市","嘉義縣","台南市","高雄市","屏東縣","宜蘭縣","花蓮縣","台東縣","澎湖縣","金門縣","連江縣"};
     //設定天氣介面
     public WeatherFrame(){
         super("今日氣溫");
@@ -27,13 +31,15 @@ public class WeatherFrame extends JFrame{
         place = new JLabel("輸入查詢地址");
         place.setFont(new Font("Serif", Font.PLAIN, 20));
         location = new JTextField();
+        cityComboBox = new JComboBox<String>(city);
         goSearch = new JButton("查詢");
-        
+
         p = new JPanel();
         p.setLayout(new FlowLayout());
         p.add(place);
         add(p);
-        add(location);
+        //add(location);
+        add(cityComboBox);
 
         otherp = new JPanel();
         otherp.setLayout(new FlowLayout());
@@ -48,16 +54,17 @@ public class WeatherFrame extends JFrame{
     //設定視窗資訊
     public static void main(String[] args){
         WeatherFrame wFrame = new WeatherFrame();
-		wFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		wFrame.setSize(400, 200); // set frame size
-		wFrame.setVisible(true);
+        wFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        wFrame.setSize(400, 200); // set frame size
+        wFrame.setVisible(true);
     }
 
     private class WeatherHandler implements ActionListener{
         //如按下查詢按鈕，去取的使用者輸入在location地區在去查詢
         @Override
         public void actionPerformed(ActionEvent event){
-            String s = location.getText();
+            String s = (cityComboBox.getSelectedItem()).toString();
+            Spiderweather a=new Spiderweather(s);
             System.out.printf("%s%n",s);
         }
 
