cc2b8046b562334bd563560f91e6d156f8ec21c7
vivian-usr <73873274+vivian-usr@users.noreply.github.com>
2021-05-29 21:20:22 +0800

    Update WeatherFrame.java

--- WeatherFrame.java
+++ WeatherFrame.java
@@ -21,8 +21,8 @@ public class WeatherFrame extends JFrame{
     private JPanel otherp;
     private final JComboBox<String> cityComboBox;
     private static final String[] city =
-            {"基隆市", "台北市", "新北市","桃園市","新竹市","新竹縣","苗栗縣","台中市","彰化縣","南投縣"
-            ,"雲林縣","嘉義市","嘉義縣","台南市","高雄市","屏東縣","宜蘭縣","花蓮縣","台東縣","澎湖縣","金門縣","連江縣"};
+            {"基隆市", "臺北市", "新北市","桃園市","新竹市","新竹縣","苗栗縣","臺中市","彰化縣","南投縣"
+                    ,"雲林縣","嘉義市","嘉義縣","臺南市","高雄市","屏東縣","宜蘭縣","花蓮縣","臺東縣","澎湖縣","金門縣","連江縣"};
     //設定天氣介面
     public WeatherFrame(){
         super("今日氣溫");
@@ -50,7 +50,6 @@ public class WeatherFrame extends JFrame{
         goSearch.addActionListener(weatherHandler);
 
     }
-
     //設定視窗資訊
     public static void main(String[] args){
         WeatherFrame wFrame = new WeatherFrame();
@@ -65,7 +64,7 @@ public class WeatherFrame extends JFrame{
         public void actionPerformed(ActionEvent event){
             String s = (cityComboBox.getSelectedItem()).toString();
             Spiderweather a=new Spiderweather(s);
-            System.out.printf("%s%n",s);
+            System.out.printf("%s%n",a.getWeather());
         }
 
     }
